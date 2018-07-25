5503adda75a7c090f6101bad68ee612bf89bf322
napolreon2 <napolreon2@naver.com>
2018-07-25 16:01:06 +0900

    1

--- TATTEST/src/main/java/com/kh/TAT/adminPage/model/dao/AdminDao.java
+++ TATTEST/src/main/java/com/kh/TAT/adminPage/model/dao/AdminDao.java
@@ -1,5 +1,17 @@
 package com.kh.TAT.adminPage.model.dao;
 
+import java.util.List;
+
+import com.kh.TAT.adminPage.model.vo.PagingVo;
+import com.kh.TAT.common.model.vo.Member;
+
+
 public interface AdminDao {
+
+	List<Member> selectMemberList(PagingVo paging);
+
+	int selectTotalPaging();
+
+	
 
 }
