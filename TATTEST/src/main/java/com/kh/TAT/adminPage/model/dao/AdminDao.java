af803aaf145655b56ba260333d80f5780a968ed1
Kim Han Gyeol <rlagksruf95@naver.com>
2018-08-01 21:28:45 +0900

    commit 9:28

--- TATTEST/src/main/java/com/kh/TAT/adminPage/model/dao/AdminDao.java
+++ TATTEST/src/main/java/com/kh/TAT/adminPage/model/dao/AdminDao.java
@@ -3,16 +3,48 @@ package com.kh.TAT.adminPage.model.dao;
 import java.util.List;
 
 import com.kh.TAT.adminPage.model.vo.PagingVo;
+import com.kh.TAT.adminPage.model.vo.aPayment;
+import com.kh.TAT.common.model.vo.Edit;
+import com.kh.TAT.common.model.vo.FaqBoard;
 import com.kh.TAT.common.model.vo.Member;
 
 
 public interface AdminDao {
 
+	// Member
 	List<Member> selectMemberList(PagingVo paging);
 
-	int selectTotalPaging();
+	int MemberTotalPaging();
 
-	int totalPagingCount(int start);
+	int memberTotalPagingCount(int start);
+	
+	// Payment
+
+	int paymentTotalPagingCount(int start);
+
+	int PaymentTotalPaging();
+
+	List<aPayment> selectPaymentList(PagingVo paging);
+	
+	// Project
+
+	int projectTotalPagingCount(int start);
+
+	List<Edit> selectProjectList(PagingVo paging);
+
+	int ProjectTotalPaging();
+	
+	// Faq
+
+	int faqTotalPagingCount(int start);
+
+	List<FaqBoard> selectFaqList(PagingVo paging);
+
+	int FaqTotalPaging();
+
+	int insertFaqQuestion(FaqBoard faqboard);
+
+	List<FaqBoard> selectFaqSelect();
 
 	
 
