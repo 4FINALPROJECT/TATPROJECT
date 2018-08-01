ab193bd552be410876999989d525a95db5994ec5
wheretanpopo <36845403+wheretanpopo@users.noreply.github.com>
2018-08-01 21:18:23 +0900

    송대섭 커밋앤 푸쉬

--- TATTEST/src/main/java/com/kh/TAT/mainPage/model/dao/MainDaoImpl.java
+++ TATTEST/src/main/java/com/kh/TAT/mainPage/model/dao/MainDaoImpl.java
@@ -12,177 +12,178 @@ import com.kh.TAT.common.model.vo.Edit;
 import com.kh.TAT.common.model.vo.EditReplyBoard;
 import com.kh.TAT.common.model.vo.Member;
 import com.kh.TAT.common.model.vo.Payment;
+import com.kh.TAT.common.model.vo.QuestionAnswerBoard;
 import com.kh.TAT.common.model.vo.Template;
 import com.kh.TAT.common.model.vo.TemplateReplyBoard;
 
 @Repository
 public class MainDaoImpl implements MainDao {
-	
-	@Autowired
-	SqlSessionTemplate sqlSession;
+   
+   @Autowired
+   SqlSessionTemplate sqlSession;
 
-	@Override
-	public int insertMember(Member member) {
-		// TODO Auto-generated method stub
-		return sqlSession.insert("member.insertMember", member);
-	}
+   @Override
+   public int insertMember(Member member) {
+      // TODO Auto-generated method stub
+      return sqlSession.insert("member.insertMember", member);
+   }
 
-	@Override
-	public Member selectOneMember(String m_email) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("member.memberLogin", m_email);
-	}
+   @Override
+   public Member selectOneMember(String m_email) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("member.memberLogin", m_email);
+   }
 
-	@Override
-	public int checkemailDuplicate(String m_email) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("member.checkemailDuplicate", m_email);
-	}
+   @Override
+   public int checkemailDuplicate(String m_email) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("member.checkemailDuplicate", m_email);
+   }
 
-	@Override
-	public int changePWD(Member m) {
-		// TODO Auto-generated method stub
-		return sqlSession.update("member.changePWD" ,m);
-	}
+   @Override
+   public int changePWD(Member m) {
+      // TODO Auto-generated method stub
+      return sqlSession.update("member.changePWD" ,m);
+   }
 
-	@Override
-	public String getPw(Map<String, Object> paramMap) {
-		// TODO Auto-generated method stub
-		return (String) sqlSession.selectOne("member.emailCheck", paramMap);
-	}
+   @Override
+   public String getPw(Map<String, Object> paramMap) {
+      // TODO Auto-generated method stub
+      return (String) sqlSession.selectOne("member.emailCheck", paramMap);
+   }
 
-	@Override
-	public int insertPayment(Payment p) {
-		// TODO Auto-generated method stub
-		return sqlSession.insert("payment.insertPayment", p);
-	}
+   @Override
+   public int insertPayment(Payment p) {
+      // TODO Auto-generated method stub
+      return sqlSession.insert("payment.insertPayment", p);
+   }
 
-	@Override
-	public void updateUsable(String m_code) {
-		// TODO Auto-generated method stub
-		sqlSession.update("member.updateUsable", m_code);
-	}
+   @Override
+   public void updateUsable(String m_code) {
+      // TODO Auto-generated method stub
+      sqlSession.update("member.updateUsable", m_code);
+   }
 
-	@Override
-	public Member emailCheck(String f_email) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("member.memberLogin", f_email);
-	}
+   @Override
+   public Member emailCheck(String f_email) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("member.memberLogin", f_email);
+   }
 
-	@Override
-	public int insertFMember(Member m) {
-		// TODO Auto-generated method stub
-		return sqlSession.insert("member.insertFmember", m);
-	}
+   @Override
+   public int insertFMember(Member m) {
+      // TODO Auto-generated method stub
+      return sqlSession.insert("member.insertFmember", m);
+   }
 
-	@Override
-	public int insertGMember(Member m) {
-		// TODO Auto-generated method stub
-		return sqlSession.insert("member.insertGmember", m);
-	}
+   @Override
+   public int insertGMember(Member m) {
+      // TODO Auto-generated method stub
+      return sqlSession.insert("member.insertGmember", m);
+   }
 
-	@Override
-	public List<Map<String, String>> selectAnswerBoard() {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("questionAnswerBoard.selectBoard");
-	}
+   @Override
+   public List<Map<String, String>> selectAnswerBoard() {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("questionAnswerBoard.selectBoard");
+   }
 
-	@Override
-	public int ddaySelect(String m_code) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("payment.ddayQuery", m_code);
-	}
+   @Override
+   public int ddaySelect(String m_code) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("payment.ddayQuery", m_code);
+   }
 
-	@Override
-	public List<Map<String, String>> selectTemplateBoard() {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("template.selectTemplate");
-	}
+   @Override
+   public List<Map<String, String>> selectTemplateBoard() {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("template.selectTemplate");
+   }
 
-	@Override
-	public Template tempDetail(String t_code) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("template.selectDetail", t_code);
-	}
+   @Override
+   public Template tempDetail(String t_code) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("template.selectDetail", t_code);
+   }
 
-	@Override
-	public List<Map<String, String>> replyBoard(String t_code) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("templateReplyBoard.selectReplyBoard", t_code);
-	}
+   @Override
+   public List<Map<String, String>> replyBoard(String t_code) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("templateReplyBoard.selectReplyBoard", t_code);
+   }
 
-	@Override
-	public int insertReplyBoard(TemplateReplyBoard ter) {
-		// TODO Auto-generated method stub
-		return sqlSession.insert("templateReplyBoard.insertReplyBoard", ter);
-	}
+   @Override
+   public int insertReplyBoard(TemplateReplyBoard ter) {
+      // TODO Auto-generated method stub
+      return sqlSession.insert("templateReplyBoard.insertReplyBoard", ter);
+   }
 
-	@Override
-	public Member selectOneMCode(String m_code) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("member.memberCode", m_code);
+   @Override
+   public Member selectOneMCode(String m_code) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("member.memberCode", m_code);
+   }
 
-	}
+   @Override
+   public void DeleteReply(TemplateReplyBoard ter) {
+      // TODO Auto-generated method stub
+      sqlSession.delete("templateReplyBoard.deleteReplyBoard", ter);
+      
+   }
 
-	@Override
-	public void DeleteReply(TemplateReplyBoard ter) {
-		// TODO Auto-generated method stub
-		sqlSession.delete("templateReplyBoard.deleteReplyBoard", ter);
-		
-	}
+   @Override
+   public void UpdateReply(TemplateReplyBoard ter) {
+      // TODO Auto-generated method stub
+      sqlSession.update("templateReplyBoard.updateReplyBoard", ter);
+   }
 
-	@Override
-	public void UpdateReply(TemplateReplyBoard ter) {
-		// TODO Auto-generated method stub
-		sqlSession.update("templateReplyBoard.updateReplyBoard", ter);
-	}
+   @Override
+   public List<Map<String, String>> selectFaqBoard() {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("faqBoard.selectFaqBoard");
+   }
 
-	@Override
-	public List<Map<String, String>> selectFaqBoard() {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("faqBoard.selectFaqBoard");
-	}
+   @Override
+   public List<Map<String, String>> selectEditBoard() {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("edit.selectEdit");
+   }
 
-	@Override
-	public List<Map<String, String>> selectEditBoard() {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("edit.selectEdit");
-	}
+   @Override
+   public Edit editDetail(String e_code) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectOne("edit.selectDetailEdit", e_code);
+   }
 
-	@Override
-	public Edit editDetail(String e_code) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectOne("edit.selectDetailEdit", e_code);
-	}
+   @Override
+   public List<Map<String, String>> EditreplyBoard(String e_code) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("editReplyBoard.selectEditReplyBoard", e_code);
+   }
 
-	@Override
-	public List<Map<String, String>> EditreplyBoard(String e_code) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("editReplyBoard.selectEditReplyBoard", e_code);
-	}
+   @Override
+   public int insertEditReplyBoard(EditReplyBoard er) {
+      // TODO Auto-generated method stub
+      return sqlSession.insert("editReplyBoard.insertEditReplyBoard", er);
+   }
 
-	@Override
-	public int insertEditReplyBoard(EditReplyBoard er) {
-		// TODO Auto-generated method stub
-		return sqlSession.insert("editReplyBoard.insertEditReplyBoard", er);
-	}
+   @Override
+   public void DeleteEditReply(EditReplyBoard er) {
+      // TODO Auto-generated method stub
+      sqlSession.delete("editReplyBoard.deleteEditReplyBoard", er);
+   }
 
-	@Override
-	public void DeleteEditReply(EditReplyBoard er) {
-		// TODO Auto-generated method stub
-		sqlSession.delete("editReplyBoard.deleteEditReplyBoard", er);
-	}
+   @Override
+   public void UpdateEditReply(EditReplyBoard er) {
+      // TODO Auto-generated method stub
+      sqlSession.update("editReplyBoard.updateEditReplyBoard", er);
+   }
 
-	@Override
-	public void UpdateEditReply(EditReplyBoard er) {
-		// TODO Auto-generated method stub
-		sqlSession.update("editReplyBoard.updateEditReplyBoard", er);
-	}
+   @Override
+   public List<Map<String, String>> selectEdit(Edit newedit) {
+      // TODO Auto-generated method stub
+      return sqlSession.selectList("edit.selectEditOne", newedit);
+   }
 
-	@Override
-	public List<Map<String, String>> selectEdit(Edit newedit) {
-		// TODO Auto-generated method stub
-		return sqlSession.selectList("edit.selectEditOne", newedit);
-	}	
 
 }
--- TATTEST/src/main/java/com/kh/TAT/myPage/model/service/MyService.java
+++ TATTEST/src/main/java/com/kh/TAT/myPage/model/service/MyService.java
@@ -3,12 +3,13 @@ package com.kh.TAT.myPage.model.service;
 import java.util.List;
 
 import com.kh.TAT.common.model.vo.Member;
+import com.kh.TAT.common.model.vo.QuestionAnswerBoard;
 import com.kh.TAT.myPage.model.vo.MyPayment;
 import com.kh.TAT.myPage.model.vo.MyProject;
 
 public interface MyService {
 
-	//회원 정보 관련 
+	// 회원 정보 관련 
 	Member selectOneMemberCode(String m_code);
 
 	int updateProfile(Member m);
@@ -24,10 +25,16 @@ public interface MyService {
 	int updateEmail(Member m);
 
 	
-	//결제 정보 관련 
+	// 결제 정보 관련 
 	MyPayment selectOnePayment(String m_code);
 
 	List<MyPayment> selectListPayment(String m_code);
+
+	
+	// 1:1 게시판 관련
+	List<QuestionAnswerBoard> selectQuestionBoard(String m_code);
+
+	List<QuestionAnswerBoard> widgetComment(String m_code);
 
 	
 
