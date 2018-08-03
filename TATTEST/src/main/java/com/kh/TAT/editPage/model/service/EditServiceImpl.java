f7534828e970a584d3856c25bb57cdac816510fc
KYHK <dbsghks884@naver.com>
2018-08-03 22:47:54 +0900

    탈모 가즈아~~!

--- TATTEST/src/main/java/com/kh/TAT/editPage/model/service/EditServiceImpl.java
+++ TATTEST/src/main/java/com/kh/TAT/editPage/model/service/EditServiceImpl.java
@@ -3,6 +3,7 @@ package com.kh.TAT.editPage.model.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.TAT.common.model.vo.Edit;
 import com.kh.TAT.common.model.vo.Member;
 import com.kh.TAT.editPage.model.dao.EditDao;
 
@@ -18,6 +19,13 @@ public class EditServiceImpl implements EditService {
 		return editD.memberSelectPayment(m_code);
 	}
 
+	@Override
+	public Edit projectSelectOne(String e_code) {
+		
+		return editD.projectSelectOne(e_code);
+	}
+
+	
 
 }
 
